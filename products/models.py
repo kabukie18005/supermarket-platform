855a00dad56da8d8addd8ad587232334f3d55348
Christabel Chrappah <christabelk2005@gmail.com>
2026-05-12 20:07:40 +0000

    fix: use Cloudinary for product images

--- products/models.py
+++ products/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-
+from cloudinary.models import CloudinaryField
 class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.CharField(max_length=100)
     stock = models.IntegerField(default=0)
     description = models.TextField(blank=True)
-    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    image = CloudinaryField('image', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
